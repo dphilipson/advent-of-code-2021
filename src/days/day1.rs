d6b6c82c475abe0ee5aad15b8bb50c4eaa2fef1e
David Philipson <david.philipson@gmail.com>
2021-11-30 21:13:44 -0800

    Day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,11 +1,19 @@
 use crate::harness::input::RawInput;
 
-pub fn solve_part1(input: RawInput) -> i32 {
-    let lines = input.per_line(|line| line.single::<String>());
-    todo!("{}", lines.len()) // Suppresses unused variable warning.
+pub fn solve_part1(input: RawInput) -> usize {
+    let depths = input.per_line(|line| line.single::<usize>());
+    count_increases(&depths)
 }
 
-pub fn solve_part2(input: RawInput) -> i32 {
-    let lines = input.per_line(|line| line.single::<String>());
-    todo!("{}", lines.len()) // Suppresses unused variable warning.
+pub fn solve_part2(input: RawInput) -> usize {
+    let depths = input.per_line(|line| line.single::<usize>());
+    let mut windows: Vec<usize> = vec![];
+    for i in 2..depths.len() {
+        windows.push(depths[i] + depths[i - 1] + depths[i - 2])
+    }
+    count_increases(&windows)
+}
+
+fn count_increases(ns: &[usize]) -> usize {
+    (1..ns.len()).filter(|&i| ns[i] > ns[i - 1]).count()
 }
