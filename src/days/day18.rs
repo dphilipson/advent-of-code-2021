e8ab0ecdda495c4c8c83254d52855d0affdbbeb3
David Philipson <david.philipson@gmail.com>
2021-12-18 02:43:30 -0800

    Cleaner but slower Day 18

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,210 +1,173 @@
 use crate::harness::input::RawInput;
 use std::cmp;
+use std::str::FromStr;
 
 pub fn solve_part1(input: RawInput) -> u32 {
-    let lines = input.per_line(|line| line.single::<String>());
-    let mut acc = parse_tokens(&lines[0]);
-    acc = reduce_snailfish_number(acc);
-    for line in &lines[1..] {
-        let tokens = parse_tokens(line);
-        acc = add_snailfish_numbers(&acc, &tokens);
-        acc = reduce_snailfish_number(acc);
-    }
-    let (tree, _) = parse_tree(&acc);
-    tree.get_value()
+    let snailfish_nums = input.per_line(|line| line.single::<SnailfishNum>());
+    let sum = snailfish_nums.into_iter().reduce(|a, b| a.add(&b)).unwrap();
+    sum.get_magnitude()
 }
 
 pub fn solve_part2(input: RawInput) -> u32 {
-    let lines = input.per_line(|line| line.single::<String>());
-    let mut best = 0;
-    for i in 0..lines.len() {
-        for j in 0..lines.len() {
+    let snailfish_nums = input.per_line(|line| line.single::<SnailfishNum>());
+    let mut best_magnitude = 0;
+    for i in 0..snailfish_nums.len() {
+        for j in 0..snailfish_nums.len() {
             if i != j {
-                let sum = add_snailfish_numbers(&parse_tokens(&lines[i]), &parse_tokens(&lines[j]));
-                let reduced_sum = reduce_snailfish_number(sum);
-                let (tree, _) = parse_tree(&reduced_sum);
-                best = cmp::max(best, tree.get_value());
+                let magnitude = snailfish_nums[i].add(&snailfish_nums[j]).get_magnitude();
+                best_magnitude = cmp::max(best_magnitude, magnitude);
             }
         }
     }
-    best
+    best_magnitude
 }
 
 #[derive(Copy, Clone, Debug)]
 enum Token {
-    Open,
-    Close,
-    Comma,
+    PairStart,
     Num(u32),
 }
 
 impl Token {
     fn downcast_to_num(self) -> u32 {
-        if let Token::Num(n) = self {
-            n
-        } else {
-            panic!("Token was not a number. Was: {:?}", self)
+        match self {
+            Self::Num(n) => n,
+            _ => panic!("Cannot downcast {:?} to number.", self),
         }
     }
-}
-
-impl ToString for Token {
-    fn to_string(&self) -> String {
-        match &self {
-            Token::Open => "[".to_owned(),
-            Token::Close => "]".to_owned(),
-            Token::Comma => ",".to_owned(),
-            Token::Num(n) => n.to_string(),
-        }
-    }
-}
-
-fn parse_tokens(s: &str) -> Vec<Token> {
-    let mut bytes: &[u8] = &s.bytes().collect::<Vec<_>>();
-    let mut result = vec![];
-    while let Some((token, remaining_bytes)) = parse_token(bytes) {
-        result.push(token);
-        bytes = remaining_bytes;
-    }
-    result
-}
-
-fn tokens_to_string(tokens: &[Token]) -> String {
-    tokens.iter().map(|token| token.to_string()).collect()
-}
-
-fn parse_token(mut bytes: &[u8]) -> Option<(Token, &[u8])> {
-    if let Some(&byte) = bytes.first() {
-        match byte {
-            b'[' => Some((Token::Open, &bytes[1..])),
-            b']' => Some((Token::Close, &bytes[1..])),
-            b',' => Some((Token::Comma, &bytes[1..])),
-            _ => {
-                let mut n = 0;
-                while (b'0'..=b'9').contains(&bytes[0]) {
-                    n = n * 10 + (bytes[0] - b'0') as u32;
-                    bytes = &bytes[1..];
-                }
-                Some((Token::Num(n), bytes))
-            }
-        }
-    } else {
-        None
-    }
-}
-
-fn add_snailfish_numbers(tokens1: &[Token], tokens2: &[Token]) -> Vec<Token> {
-    let mut result = Vec::with_capacity(tokens1.len() + tokens2.len() + 3);
-    result.push(Token::Open);
-    result.extend(tokens1);
-    result.push(Token::Comma);
-    result.extend(tokens2);
-    result.push(Token::Close);
-    result
-}
-
-fn reduce_snailfish_number(mut tokens: Vec<Token>) -> Vec<Token> {
-    while let Some(next_tokens) = apply_step(&tokens) {
-        tokens = next_tokens;
-    }
-    tokens
-}
-
-/// Returns None if the tokens are settled.
-fn apply_step(tokens: &[Token]) -> Option<Vec<Token>> {
-    if let Some(i) = find_exploding_start(tokens) {
-        Some(explode_at(tokens, i))
-    } else if let Some(i) = find_splitting_start(tokens) {
-        Some(split_at(tokens, i))
-    } else {
-        None
-    }
-}
-
-fn find_exploding_start(tokens: &[Token]) -> Option<usize> {
-    let mut depth_count = 0;
-    for (i, &token) in tokens.iter().enumerate() {
-        match token {
-            Token::Open => {
-                depth_count += 1;
-                if depth_count > 4 {
-                    return Some(i);
-                }
-            }
-            Token::Close => depth_count -= 1,
-            _ => (),
-        }
-    }
-    None
-}
-
-fn find_splitting_start(tokens: &[Token]) -> Option<usize> {
-    tokens.iter().position(|&token| match token {
-        Token::Num(n) => n >= 10,
-        _ => false,
-    })
-}
-
-fn explode_at(tokens: &[Token], i: usize) -> Vec<Token> {
-    let mut result = Vec::with_capacity(tokens.len() - 4);
-    let left = tokens[i + 1].downcast_to_num();
-    let right = tokens[i + 3].downcast_to_num();
-    result.extend(&tokens[..i]);
-    result.push(Token::Num(0));
-    result.extend(&tokens[(i + 5)..]);
-    for i in (0..i).rev() {
-        if let Token::Num(n) = result[i] {
-            result[i] = Token::Num(n + left);
-            break;
-        }
-    }
-    for i in i + 1..result.len() {
-        if let Token::Num(n) = result[i] {
-            result[i] = Token::Num(n + right);
-            break;
-        }
-    }
-    result
-}
-
-fn split_at(tokens: &[Token], i: usize) -> Vec<Token> {
-    let mut result = Vec::with_capacity(tokens.len() + 4);
-    let n = tokens[i].downcast_to_num();
-    result.extend(&tokens[..i]);
-    result.extend([
-        Token::Open,
-        Token::Num(n / 2),
-        Token::Comma,
-        Token::Num((n + 1) / 2),
-        Token::Close,
-    ]);
-    result.extend(&tokens[i + 1..]);
-    result
 }
 
 #[derive(Debug)]
-enum Node {
-    Pair(Box<Node>, Box<Node>),
-    Num(u32),
-}
+struct SnailfishNum(Vec<Token>);
 
-impl Node {
-    fn get_value(&self) -> u32 {
-        match self {
-            Node::Pair(left, right) => 3 * left.get_value() + 2 * right.get_value(),
-            Node::Num(n) => *n,
-        }
+impl FromStr for SnailfishNum {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tokens = s
+            .as_bytes()
+            .iter()
+            .filter_map(|&b| match b {
+                b'[' => Some(Token::PairStart),
+                b'0'..=b'9' => Some(Token::Num((b - b'0') as u32)),
+                _ => None,
+            })
+            .collect();
+        Ok(Self(tokens))
     }
 }
 
-fn parse_tree(tokens: &[Token]) -> (Node, &[Token]) {
-    match tokens[0] {
-        Token::Num(n) => (Node::Num(n), &tokens[1..]),
-        Token::Open => {
-            let (left, tokens) = parse_tree(&tokens[1..]);
-            let (right, tokens) = parse_tree(&tokens[1..]);
-            (Node::Pair(Box::new(left), Box::new(right)), &tokens[1..])
+impl SnailfishNum {
+    fn add(&self, rhs: &Self) -> Self {
+        let mut tokens = Vec::with_capacity(self.0.len() + rhs.0.len() + 1);
+        tokens.push(Token::PairStart);
+        tokens.extend(&self.0);
+        tokens.extend(&rhs.0);
+        let mut result = Self(tokens);
+        result.reduce();
+        result
+    }
+
+    fn reduce(&mut self) {
+        while self.reduce_once() {}
+    }
+
+    fn reduce_once(&mut self) -> bool {
+        if let Some(i) = self.find_explode_index() {
+            self.explode_at(i);
+            true
+        } else if let Some(i) = self.find_split_index() {
+            self.split_at(i);
+            true
+        } else {
+            false
         }
-        _ => unreachable!(),
+    }
+
+    fn find_explode_index(&self) -> Option<usize> {
+        let mut depth = 0;
+        // true on stack represents a number, false a pairstart.
+        let mut stack = vec![];
+        for (i, &token) in self.0.iter().enumerate() {
+            match token {
+                Token::PairStart => {
+                    depth += 1;
+                    if depth > 4 {
+                        return Some(i);
+                    }
+                    stack.push(false);
+                }
+                Token::Num(_) => {
+                    stack.push(true);
+                    while stack.len() > 2 && stack[stack.len() - 1] && stack[stack.len() - 2] {
+                        for _ in 0..3 {
+                            stack.pop();
+                        }
+                        stack.push(true);
+                        depth -= 1;
+                    }
+                }
+            }
+        }
+        None
+    }
+
+    fn find_split_index(&self) -> Option<usize> {
+        self.0.iter().position(|&token| match token {
+            Token::Num(n) => n >= 10,
+            _ => false,
+        })
+    }
+
+    fn explode_at(&mut self, i: usize) {
+        let left = self.0[i + 1].downcast_to_num();
+        let right = self.0[i + 2].downcast_to_num();
+        self.0.splice(i..i + 3, [Token::Num(0)]);
+        for j in (0..i).rev() {
+            match self.0[j] {
+                Token::Num(n) => {
+                    self.0[j] = Token::Num(n + left);
+                    break;
+                }
+                _ => (),
+            }
+        }
+        for j in i + 1..self.0.len() {
+            match self.0[j] {
+                Token::Num(n) => {
+                    self.0[j] = Token::Num(n + right);
+                    break;
+                }
+                _ => (),
+            }
+        }
+    }
+
+    fn split_at(&mut self, i: usize) {
+        let n = self.0[i].downcast_to_num();
+        self.0.splice(
+            i..i + 1,
+            [Token::PairStart, Token::Num(n / 2), Token::Num((n + 1) / 2)],
+        );
+    }
+
+    fn get_magnitude(&self) -> u32 {
+        let mut stack = vec![];
+        for &token in &self.0 {
+            stack.push(token);
+            while stack.len() > 2 {
+                if let Token::Num(top) = stack[stack.len() - 1] {
+                    if let Token::Num(second_top) = stack[stack.len() - 2] {
+                        for _ in 0..3 {
+                            stack.pop();
+                        }
+                        stack.push(Token::Num(2 * top + 3 * second_top));
+                        continue;
+                    }
+                }
+                break;
+            }
+        }
+        stack[0].downcast_to_num()
     }
 }
