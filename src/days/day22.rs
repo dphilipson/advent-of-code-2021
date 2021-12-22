bf0217e552f20408cebc397b4604eaa81ea5ea66
David Philipson <david.philipson@gmail.com>
2021-12-21 22:33:07 -0800

    Cleaned up Day 22

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -2,63 +2,46 @@ use crate::harness::input::RawInput;
 use crate::regex;
 use crate::util::re::parse_with_regex;
 use ndarray::Array3;
-use std::collections::HashMap;
+use std::collections::{BTreeSet, HashMap};
 use std::str::FromStr;
 use std::{cmp, error};
 
-pub fn solve_part1(input: RawInput) -> usize {
-    let instructions = input.per_line(|line| line.single::<Instruction>());
-    let mut cubes = Array3::default((101, 101, 101));
-    for instruction in instructions {
-        apply_instruction(&mut cubes, instruction);
-    }
-    cubes.iter().filter(|&&b| b).count()
+pub fn solve_part1(input: RawInput) -> i64 {
+    solve(input, true)
 }
 
 pub fn solve_part2(input: RawInput) -> i64 {
-    let instructions = input.per_line(|line| line.single::<Instruction>());
-    let mut x_breakpoints = Vec::with_capacity(instructions.len() * 2);
-    let mut y_breakpoints = Vec::with_capacity(instructions.len() * 2);
-    let mut z_breakpoints = Vec::with_capacity(instructions.len() * 2);
-    for instruction in &instructions {
-        x_breakpoints.extend([instruction.x_min, instruction.x_max + 1]);
-        y_breakpoints.extend([instruction.y_min, instruction.y_max + 1]);
-        z_breakpoints.extend([instruction.z_min, instruction.z_max + 1]);
+    solve(input, false)
+}
+
+pub fn solve(input: RawInput, trim_region: bool) -> i64 {
+    let mut steps = input.per_line(|line| line.single::<Step>());
+    if trim_region {
+        steps = steps
+            .into_iter()
+            .map(|step| step.trim_to_initialization_region())
+            .collect();
     }
-    x_breakpoints.sort();
-    y_breakpoints.sort();
-    z_breakpoints.sort();
-    let x_indices_by_breakpoint = x_breakpoints
-        .iter()
-        .enumerate()
-        .map(|(i, &x)| (x, i))
-        .collect::<HashMap<_, _>>();
-    let y_indices_by_breakpoint = y_breakpoints
-        .iter()
-        .enumerate()
-        .map(|(i, &y)| (y, i))
-        .collect::<HashMap<_, _>>();
-    let z_indices_by_breakpoint = z_breakpoints
-        .iter()
-        .enumerate()
-        .map(|(i, &z)| (z, i))
-        .collect::<HashMap<_, _>>();
+    let [x_breakpoints, y_breakpoints, z_breakpoints] = get_sorted_breakpoints(&steps);
+    let x_indices_by_breakpoint = get_indices_by_breakpoints(&x_breakpoints);
+    let y_indices_by_breakpoint = get_indices_by_breakpoints(&y_breakpoints);
+    let z_indices_by_breakpoint = get_indices_by_breakpoints(&z_breakpoints);
     let mut grid = Array3::default((
         x_breakpoints.len() - 1,
         y_breakpoints.len() - 1,
         z_breakpoints.len() - 1,
     ));
-    for instruction in instructions {
-        let x_min_index = x_indices_by_breakpoint[&instruction.x_min];
-        let x_max_index = x_indices_by_breakpoint[&(instruction.x_max + 1)];
-        let y_min_index = y_indices_by_breakpoint[&instruction.y_min];
-        let y_max_index = y_indices_by_breakpoint[&(instruction.y_max + 1)];
-        let z_min_index = z_indices_by_breakpoint[&instruction.z_min];
-        let z_max_index = z_indices_by_breakpoint[&(instruction.z_max + 1)];
+    for step in steps {
+        let x_min_index = x_indices_by_breakpoint[&step.x_min];
+        let x_max_index = x_indices_by_breakpoint[&(step.x_max + 1)];
+        let y_min_index = y_indices_by_breakpoint[&step.y_min];
+        let y_max_index = y_indices_by_breakpoint[&(step.y_max + 1)];
+        let z_min_index = z_indices_by_breakpoint[&step.z_min];
+        let z_max_index = z_indices_by_breakpoint[&(step.z_max + 1)];
         for x in x_min_index..x_max_index {
             for y in y_min_index..y_max_index {
                 for z in z_min_index..z_max_index {
-                    grid[[x, y, z]] = instruction.is_on;
+                    grid[[x, y, z]] = step.is_on;
                 }
             }
         }
@@ -79,7 +62,7 @@ pub fn solve_part2(input: RawInput) -> i64 {
 }
 
 #[derive(Copy, Clone, Debug)]
-struct Instruction {
+struct Step {
     is_on: bool,
     x_min: i64,
     x_max: i64,
@@ -89,13 +72,13 @@ struct Instruction {
     z_max: i64,
 }
 
-impl FromStr for Instruction {
+impl FromStr for Step {
     type Err = Box<dyn error::Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let re = regex!(r"^(.+) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)$");
         let result: (String, i64, i64, i64, i64, i64, i64) = parse_with_regex(re, s)?;
-        Ok(Instruction {
+        Ok(Step {
             is_on: &result.0 == "on",
             x_min: result.1,
             x_max: result.2,
@@ -107,18 +90,40 @@ impl FromStr for Instruction {
     }
 }
 
-fn apply_instruction(grid: &mut Array3<bool>, instruction: Instruction) {
-    let x_min = cmp::max(instruction.x_min, -50) + 50;
-    let x_max = cmp::min(instruction.x_max, 50) + 50;
-    let y_min = cmp::max(instruction.y_min, -50) + 50;
-    let y_max = cmp::min(instruction.y_max, 50) + 50;
-    let z_min = cmp::max(instruction.z_min, -50) + 50;
-    let z_max = cmp::min(instruction.z_max, 50) + 50;
-    for x in x_min..=x_max {
-        for y in y_min..=y_max {
-            for z in z_min..=z_max {
-                grid[[x as usize, y as usize, z as usize]] = instruction.is_on;
-            }
+impl Step {
+    fn trim_to_initialization_region(self) -> Self {
+        Self {
+            is_on: self.is_on,
+            x_min: cmp::max(-50, self.x_min),
+            x_max: cmp::min(50, self.x_max),
+            y_min: cmp::max(-50, self.y_min),
+            y_max: cmp::min(50, self.y_max),
+            z_min: cmp::max(-50, self.z_min),
+            z_max: cmp::min(50, self.z_max),
         }
     }
+}
+
+fn get_sorted_breakpoints(steps: &[Step]) -> [Vec<i64>; 3] {
+    let mut x_breakpoints = BTreeSet::new();
+    let mut y_breakpoints = BTreeSet::new();
+    let mut z_breakpoints = BTreeSet::new();
+    for step in steps {
+        x_breakpoints.extend([step.x_min, step.x_max + 1]);
+        y_breakpoints.extend([step.y_min, step.y_max + 1]);
+        z_breakpoints.extend([step.z_min, step.z_max + 1]);
+    }
+    [
+        Vec::from_iter(x_breakpoints),
+        Vec::from_iter(y_breakpoints),
+        Vec::from_iter(z_breakpoints),
+    ]
+}
+
+fn get_indices_by_breakpoints(breakpoints: &[i64]) -> HashMap<i64, usize> {
+    breakpoints
+        .iter()
+        .enumerate()
+        .map(|(i, &x)| (x, i))
+        .collect()
 }
